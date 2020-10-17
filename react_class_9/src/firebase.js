9977c03e0e56aa7d8e161e22258d0cd6c1113aa6
hammad siraj <hs@MBPs-MacBook-Pro.local>
2020-10-18 01:03:25 +0500

    react_firebase

--- react_class_9/src/firebase.js
+++ react_class_9/src/firebase.js
@@ -1,14 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyC-puZEJvZqRw8z0wzog44DI34C_1qjfAw",
-  authDomain: "module-b-crud.firebaseapp.com",
-  databaseURL: "https://module-b-crud.firebaseio.com",
-  projectId: "module-b-crud",
-  storageBucket: "module-b-crud.appspot.com",
-  messagingSenderId: "166128298776",
-  appId: "1:166128298776:web:d2fdde825cdca1c05e0c6d",
-  measurementId: "G-TQC2VJD0QH",
+  // add your firebase project config
 };
 
 firebase.initializeApp(firebaseConfig);
